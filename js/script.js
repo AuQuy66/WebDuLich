8f5bad07fe28373b0c66a1473f241cc8b38623cb
TrangNhung19 <94165318+TrangNhung19@users.noreply.github.com>
2023-07-14 00:59:52 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ const header=document.querySelector("header");
 window.addEventListener("scroll",function() {
     header.classList.toggle("stick",window.scrollY>60)
 });
+
+const carousel = document.querySelector(".carousel");
+firstImg = carousel.querySelectorAll("img")[0];
+buttonIcon = document.querySelectorAll(".carousel-button")
+
+let firstImgWidth = firstImg.clientWidth + 14;
+
+buttonIcon.forEach(button => {
+    button.addEventListener("click", () => {
+        carousel.scrollLeft += button.id == "left" ? -firstImgWidth : firstImgWidth;
+    })
+});
